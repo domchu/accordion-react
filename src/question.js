da2d58fc29af0a2af602489d90ed2f9ebb6cffc6
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-09 15:16:08 +0100

    text:added the icons and links

--- src/question.js
+++ src/question.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 const Question = ({ title, info }) => {
   const [showInfo, setShowInfo] = useState(false);
 
@@ -8,8 +8,7 @@ const Question = ({ title, info }) => {
       <header>
         <h4>{title}</h4>
         <button className="btn" onClick={() => setShowInfo(!showInfo)}>
-          {/* {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />} */}
-          btn
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
       {showInfo && <p>{info}</p>}
